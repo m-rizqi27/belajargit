20af34355b273756f655eaa9880866de5988c2f0
m-rizqi27 <muhammadrizqi712@gmail.com>
2020-05-07 20:29:26 +0700

    add tittle

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-height=1.0">
+  <title>Belajar Git</title>
 </head>
 <body>
 Hello World
