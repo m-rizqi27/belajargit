9b64128aac1e6568436aaa54a4c70307d40bbae3
m-rizqi27 <muhammadrizqi712@gmail.com>
2020-05-07 20:33:11 +0700

    add stylesheet

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-height=1.0">
   <title>Belajar Git</title>
+  <link rel="stylesheet" href="lib/css/stylesheet.css">
 </head>
 <body>
 Hello World
